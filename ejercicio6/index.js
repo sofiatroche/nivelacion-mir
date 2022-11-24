6592510839521e5e8922d440261fcdd8881e8f12
Sofia <sofiatroche@gmail.com>
2022-11-23 21:31:56 -0300

    retos de la actividad 6

---
+++ ejercicio6/index.js
@@ -0,0 +1,27 @@
+let pedro = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    activo: true,
+    hobbies: ["programar", "squash", "piano"],
+    estatura: 1.8
+  }
+console.log(pedro.edad);
+delete pedro.activo; 
+console.log(pedro.activo);
+
+
+for (let key in pedro) {
+    console.log(key,":", pedro[key]);
+}
+function saluda(){
+    return ("Hola, me llamo " + pedro.nombre); 
+}
+
+console.log(saluda(pedro))
+
+
+/* let llaves = Object.keys(pedro);
+for (let i=0; i < llaves.length; i++) {
+  let llave = llaves[i];
+  console.log(llave,":",pedro[llave]);
+}  */
